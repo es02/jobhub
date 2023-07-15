7ea288b4d41f35e41097d024b4307fbcccb7d49c
ArachnidsGrip <minervavine@gmail.com>
2023-07-16 05:45:45 +1000

    Basic routes for Jobs & Training

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\JobController;
 use App\Http\Controllers\ProfileController;
+// use App\Http\Controllers\TrainingController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,6 +28,20 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/job', [JobController::class, 'index'])->name('job');
+    Route::post('/job', [JobController::class, 'create'])->name('job.create');
+    Route::patch('/job/{id}', [JobController::class, 'update'])->name('job.update');
+    Route::delete('/job/{id}', [JobController::class, 'destroy'])->name('job.destroy');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/training', [TrainingController::class, 'index'])->name('training');
+    Route::post('/training', [TrainingController::class, 'create'])->name('training.create');
+    Route::patch('/training/{id}', [TrainingController::class, 'update'])->name('training.update');
+    Route::delete('/training/{id}', [TrainingController::class, 'destroy'])->name('training.destroy');
 });
 
 require __DIR__.'/auth.php';
