23c457a03c2da3e21705d23235a40b49feb339bd
Steve Ryenchindorj <tuvshinbayar.renchin@gmai.com>
2023-07-16 13:08:26 +1000

    remove empty bar when job seeker is logged in

--- resources/views/job/list.blade.php
+++ resources/views/job/list.blade.php
@@ -6,10 +6,10 @@
     </x-slot>
 
     <div class="py-12">
+        @if ($role === "Employer")
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <div class="max-w-xl">
-                    @if ($role === "Employer")
                     <section>
                         <header>
                             <h2 class="text-lg font-medium text-gray-900">
@@ -53,11 +53,11 @@
                             </form>
                             </p>
                         </header>
-                    </section>
-                    @endif    
+                    </section>  
                 </div>
-            </div>
+            </div> 
         </div>
+        @endif 
     </div>
 
     <div class="items-center max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
